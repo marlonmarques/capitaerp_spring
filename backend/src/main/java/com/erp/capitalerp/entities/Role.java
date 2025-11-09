030a8fcd4d3c6063a818a501092edea1b2399ea8
Marlon Marques <marlonmarques6@gmail.com>
2025-11-09 13:11:30 -0300

    ajuste role

--- backend/src/main/java/com/erp/capitalerp/entities/Role.java
+++ backend/src/main/java/com/erp/capitalerp/entities/Role.java
@@ -50,7 +50,7 @@ public class Role {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((users == null) ? 0 : users.hashCode());
         return result;
     }
 
@@ -63,13 +63,12 @@ public class Role {
         if (getClass() != obj.getClass())
             return false;
         Role other = (Role) obj;
-        if (id == null) {
-            if (other.id != null)
+        if (users == null) {
+            if (other.users != null)
                 return false;
-        } else if (!id.equals(other.id))
+        } else if (!users.equals(other.users))
             return false;
         return true;
     }
-
     
 }
