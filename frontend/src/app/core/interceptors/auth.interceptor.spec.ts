e24c9eafed89dfac00a2651af49073a1b2222203
Marlon Marques <marlonmarques6@gmail.com>
2025-11-09 21:12:01 -0300

    auth test

--- frontend/src/app/core/interceptors/auth.interceptor.spec.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.spec.ts
@@ -1,17 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 import { AuthInterceptor } from './auth.interceptor';
+import { AuthService } from '../services/auth.service';
 
 describe('AuthInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => new AuthInterceptor(req, next));
+  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
+  let authService: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+        {
+          provide: AuthService,
+          useValue: {
+            getToken: () => 'fake-jwt-token'
+          }
+        }
+      ]
+    });
+
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClient = TestBed.inject(HttpClient);
+    authService = TestBed.inject(AuthService);
   });
 
-  it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify(); // garante que todas as requisições foram respondidas
+  });
+
+  it('should add Authorization header', () => {
+    httpClient.get('/api/test').subscribe();
+
+    const httpRequest = httpMock.expectOne('/api/test');
+    expect(httpRequest.request.headers.has('Authorization')).toBe(true);
+    expect(httpRequest.request.headers.get('Authorization')).toBe('Bearer fake-jwt-token');
+    httpRequest.flush({ data: 'ok' });
   });
 });
